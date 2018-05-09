2c41717f522e8ee45d36a67270b011676435c392
ouyang <1697006288@qq.com>
2018-05-09 12:13:17 +0800

    update

---
+++ Classes/Config/KZWJpushModel.h
@@ -0,0 +1,39 @@
+//
+//  KZWJpushModel.h
+//  kongzhongfinancial
+//
+//  Created by ouyang on 2018/1/10.
+//  Copyright © 2018年 ouy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LPDBModel.h"
+
+@interface KZWJpushInfoModel : LPDBModel
+
+@property (nonatomic, copy) NSString *messageContent;
+@property (nonatomic, copy) NSString *userName;
+@property (nonatomic, assign) NSInteger sysType;
+@property (nonatomic, assign) BOOL isRedirct;
+@property (nonatomic, copy) NSString *targetUrl;
+@property (nonatomic, assign) NSInteger backMessageType;
+@property (nonatomic, copy) NSString *createdTime;
+
+@end
+
+@interface KZWJpushAps : LPDBModel
+
+@property (copy, nonatomic) NSString *sound;
+@property (copy, nonatomic) NSString *alert;
+@property (assign, nonatomic) NSInteger badge;
+
+@end
+
+@interface KZWJpushModel : LPDBModel
+
+@property (copy, nonatomic) NSString *extInfo;
+@property (copy, nonatomic) KZWJpushAps *aps;
+
+@end
+
+
---
+++ Classes/Config/KZWRSAenscryptString.h
@@ -0,0 +1,15 @@
+//
+//  KZWRSAenscryptString.h
+//  kongzhongfinancial
+//
+//  Created by ouyang on 2018/2/28.
+//  Copyright © 2018年 ouy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KZWRSAenscryptString : NSObject
+
++ (NSString *)signStr:(NSMutableDictionary*)dict;
+
+@end
